e6d9d6f3bb8e38e74dc86ec99f6dd889e219dec8
Mario Isernia <mario.isernia@mcc.it>
2019-06-14 17:19:37 +0200

    aggiunto metodo inutile

--- src/main/java/it/k2/formazione/bookstore/repository/BookRepository.java
+++ src/main/java/it/k2/formazione/bookstore/repository/BookRepository.java
@@ -3,5 +3,9 @@ package it.k2.formazione.bookstore.repository;
 import it.k2.formazione.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    List<Book> findAllByIsbnAndTitle(String isbn, String title);
 }
